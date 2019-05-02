49bf108502200f9f3e9a4c85306619e6be31fabb
ashcraft193 <ash12021@byui.edu>
2019-05-02 15:35:36 -0600

    Intitializing first commit from R

---
+++ Reading_Notes.R
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+Tables
+work best when the display will be used to look up individual values or the quantitative values must be precise. 
+
+Graphs
+work best when the message you wish to communicate resides in the shape of the data (that is, in patterns, trends, and exceptions). 
+
+Graphs are typically 1 quantitative scale with 1 or more categorical scales (except scatterplots which are 2 quantitave scales)
+
+3 types of categorical scales:
+  Nominal - differ in name only, no order (regions and departments)
+  Ordinal - intrinsic order, not quantitative (A,B,C; small, medium, large; poor, etc.)
+  Interval - Bins or ranges (0-99, 100-199, etc.; months)
+  
+Compared to what?
+  
+7 relationships
+  Time-Series
+  Ranking
+  Part to whole
+  Deviation
+  Distribution
+  Correlation
+  Nominal Comparison
+  
+Four types of objects that are best for encoding quantitative values
+  Points
+  Lines
+  Bars
+  Boxes
+  
+3 traits of a data scientist
+The ability to ask questions that matter
+The ability to determine that the question has been answered, part 1: logical thinking
+The ability to determine that the question has been answered, part 2: creative thinking
+
+
+Case Study 2
+dat %>% 
+  ggplot(aes()) +
+  +
+  +
+  geom_point(data = New data set)
+  
